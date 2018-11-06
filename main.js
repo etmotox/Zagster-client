9ea2aca5599bcbe23d80ef9347100d0d9cca916b
Eric Taylor <etmotox4@yahoo.com>
2018-11-06 08:36:45 -0800

    added a function

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 $(updateView)
 
+const BASE_URL = "https://zagster-service.herokuapp.com"
+
 function updateView() {
-    alert("This is Awesome!!")
+    $.getJSON(BASE_URL + "/rides/count", updateRideCount)
+}
+
+function updateRideCount(data) {
+    numberOfRides = data.count 
+    $("h2#rideCount").html(numberOfRides)
 }
