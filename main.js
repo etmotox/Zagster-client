825c27de543edd5557b9e8b6fa32f1f1add0378f
Eric Taylor <etmotox4@yahoo.com>
2018-11-13 20:12:33 -0800

    css formatting

--- main.js
+++ main.js
@@ -5,9 +5,7 @@ const BASE_URL = "https://zagster-service.herokuapp.com"
 function updateView() {
     $.getJSON(BASE_URL + "/rides/count", updateRideCount)
 }
-
 function updateRideCount(data) {
     numberOfRides = data.count 
     $("h2#rideCount").html(numberOfRides)
 }
-
