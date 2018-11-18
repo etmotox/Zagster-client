1840f033cfdb124314b1ab62ce6841ff1c6388ff
Eric Taylor <etmotox4@yahoo.com>
2018-11-17 17:55:44 -0800

    Trying to load data

--- main.js
+++ main.js
@@ -2,10 +2,31 @@ $(updateView)
 
 const BASE_URL = "https://zagster-service.herokuapp.com"
 
+var year = []
+var month = []
+
 function updateView() {
     $.getJSON(BASE_URL + "/rides/count", updateRideCount)
+    $.getJSON(BASE_URL + "/rides/count/per_year", updateYearTable)
+    $.getJSON(BASE_URL + "/rides/count/per_month", printData)
 }
 function updateRideCount(data) {
     numberOfRides = data.count 
-    $("h2#rideCount").html(numberOfRides)
+    $(".rideCount").html(numberOfRides)
+}
+function updateYearTable(data){
+    numberOfRides = data['2016']
+    $("p#2016").html(numberOfRides)
+}
+function updateYearTable(data){
+    numberOfRides = data['2017']
+    $("p#2017").html(numberOfRides)
+}
+function updateYearTable(data){
+    numberOfRides = data['2018']
+    $("p#2018").html(numberOfRides)
+}
+
+function printData(data){
+    console.log(data)
 }
