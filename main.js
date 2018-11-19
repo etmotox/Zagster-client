5d0c0ecc984bfb59e239aeed26847e9b41a3499a
Eric Taylor <etmotox4@yahoo.com>
2018-11-19 13:31:01 -0800

    took out css, still can't load chart

--- main.js
+++ main.js
@@ -3,26 +3,12 @@ $(updateView)
 const BASE_URL = "https://zagster-service.herokuapp.com"
 
 
-var ctx = document.getElementById("myChart");
-
 function updateView() {
     $.getJSON(BASE_URL + "/rides/count", updateRideCount)
     $.getJSON(BASE_URL + "/rides/count/per_year", updateYearTable)
     $.getJSON(BASE_URL + "/rides/count/per_month", updateMonthTable)
 }
-new Chart(ctx, {
-    type: 'polarArea',
-    data = {
-        datasets: [{
-            data: [10, 20, 30]
-        }],
-            labels: [
-            'Red',
-            'Yellow',
-            'Blue'
-        ]
-    }
-});
+
 function updateRideCount(data) {
     numberOfRides = data.count 
     $(".rideCount").html(numberOfRides)
